bba65063c9cb2778fac07227bebe87b628019c52
semanavasco <semanavasco@gmail.com>
2025-09-21 21:40:11 +0200

    Added 4 different Sapphire Ore types (stone, deepslate, nether, end stone)

--- src/main/java/net/svasco/examplemod/item/ModCreativeModeTabs.java
+++ src/main/java/net/svasco/examplemod/item/ModCreativeModeTabs.java
@@ -36,6 +36,10 @@ public class ModCreativeModeTabs {
                     .displayItems((pParameters, pOutput) -> {
                         pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                         pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+                        pOutput.accept(ModBlocks.SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
+                        pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
                     })
                     .build());
 }
