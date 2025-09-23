883c2ae0c298acaca145e5d119f25ddbc39d876f
semanavasco <semanavasco@gmail.com>
2025-09-23 20:17:47 +0200

    Added custom blocks tag for the metal detector item (and prepared the project for item tags as well)

---
+++ src/main/java/net/svasco/examplemod/util/ModTags.java
@@ -0,0 +1,28 @@
+package net.svasco.examplemod.util;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.svasco.examplemod.ExampleMod;
+
+public class ModTags {
+    public static class Blocks {
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(ExampleMod.MOD_ID, name));
+        }
+
+        // Tags
+        public static final TagKey<Block> METAL_DETECTOR_VALUABLES = tag("metal_detector_valuables");
+    }
+
+    public static class Items {
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(ExampleMod.MOD_ID, name));
+        }
+
+        // Tags
+    }
+}
