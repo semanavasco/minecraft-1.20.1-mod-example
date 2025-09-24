dda81191c16bc7a1df740326a006982ce2a98c34
semanavasco <semanavasco@gmail.com>
2025-09-24 19:22:15 +0200

    Added sapphire armor

---
+++ src/main/java/net/svasco/examplemod/item/ModArmorMaterials.java
@@ -0,0 +1,80 @@
+package net.svasco.examplemod.item;
+
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.svasco.examplemod.ExampleMod;
+
+import java.util.function.Supplier;
+
+public enum ModArmorMaterials implements ArmorMaterial {
+    SAPPHIRE("sapphire", 26, new int[]{5, 7, 5, 4}, 25,
+            SoundEvents.ARMOR_EQUIP_ELYTRA, 1f, 0f,
+            () -> Ingredient.of(ModItems.SAPPHIRE.get()));
+
+    private final String name;
+    private final int durabilityMultiplier;
+    private final int[] protectionAmounts;
+    private final int enchantmentValue;
+    private final SoundEvent equipSound;
+    private final float toughness;
+    private final float knockbackResistance;
+    private final Supplier<Ingredient> repairIngredient;
+
+    private static final int[] BASE_DURABILITY = {11, 16, 16, 13};
+
+    ModArmorMaterials(String name, int durabilityMultiplier, int[] protectionAmounts, int enchantmentValue,
+                      SoundEvent equipSound, float toughness, float knockbackResistance,
+                      Supplier<Ingredient> repairIngredient) {
+        this.name = name;
+        this.durabilityMultiplier = durabilityMultiplier;
+        this.protectionAmounts = protectionAmounts;
+        this.enchantmentValue = enchantmentValue;
+        this.equipSound = equipSound;
+        this.toughness = toughness;
+        this.knockbackResistance = knockbackResistance;
+        this.repairIngredient = repairIngredient;
+    }
+
+    @Override
+    public int getDurabilityForType(ArmorItem.Type pType) {
+        return BASE_DURABILITY[pType.ordinal()] * this.durabilityMultiplier;
+    }
+
+    @Override
+    public int getDefenseForType(ArmorItem.Type pType) {
+        return this.protectionAmounts[pType.ordinal()];
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return enchantmentValue;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return equipSound;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return repairIngredient.get();
+    }
+
+    @Override
+    public String getName() {
+        return ExampleMod.MOD_ID + ":" + name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return this.knockbackResistance;
+    }
+}
--- src/main/java/net/svasco/examplemod/item/ModCreativeModeTabs.java
+++ src/main/java/net/svasco/examplemod/item/ModCreativeModeTabs.java
@@ -28,12 +28,18 @@ public class ModCreativeModeTabs {
                         pOutput.accept(ModItems.RAW_SAPPHIRE.get());
                         pOutput.accept(ModItems.METAL_DETECTOR.get());
                         pOutput.accept(ModItems.PINE_CONE.get());
+
                         pOutput.accept(ModItems.SAPPHIRE_STAFF.get());
                         pOutput.accept(ModItems.SAPPHIRE_SWORD.get());
                         pOutput.accept(ModItems.SAPPHIRE_PICKAXE.get());
                         pOutput.accept(ModItems.SAPPHIRE_AXE.get());
                         pOutput.accept(ModItems.SAPPHIRE_SHOVEL.get());
                         pOutput.accept(ModItems.SAPPHIRE_HOE.get());
+
+                        pOutput.accept(ModItems.SAPPHIRE_HELMET.get());
+                        pOutput.accept(ModItems.SAPPHIRE_CHESTPLATE.get());
+                        pOutput.accept(ModItems.SAPPHIRE_LEGGINGS.get());
+                        pOutput.accept(ModItems.SAPPHIRE_BOOTS.get());
                     })
                     .build());
 
